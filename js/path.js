1f638e9bc0cdcd22c303f1cfaa6350f9f13976b2
SuperheroMovie00 <15638590102@163.com>
2019-07-22 10:12:44 +0800

    第一次提交

---
+++ js/path.js
@@ -0,0 +1,31 @@
+path="http://localhost:9090/yy-face";
+
+var time=new Date().getTime();
+var token="297e325a66f62bae0166f631b0b20002";
+
+var map={
+    timestamp:time,
+    nonce:time,
+    token:token,
+};
+
+var initmap={
+    timestamp:time,
+    nonce:time,
+    token:token,
+};
+
+function jsonSort(jsonObj) {
+    let arr = [];
+    for (var key in jsonObj) {
+        arr.push(key)
+    }
+    arr.sort();
+    let str = '';
+    for (var i in arr) {
+        if(arr[i]!='timestamp' && arr[i]!='nonce' && arr[i]!='token' && arr[i]!='sign' && jsonObj[arr[i]]!=''){
+        str += arr[i] + "=" + jsonObj[arr[i]] + "&";
+    }
+    }
+    return str;
+}
